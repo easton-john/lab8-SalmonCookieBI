754c8b481ad3ec7a7f9e6cb7d4505d4c578a219a
Easton Gorishek <ieaston26@gmail.com>
2018-05-12 14:36:11 -0700

    trying to push

--- app.js
+++ app.js
@@ -1,55 +1,43 @@
 /* globals locationList */
 
-// cookiesPerHour stores data for number of cookies needed per hour
-let cookiesPerHour = [];
+const operationHrs = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
+const totalCookiesPerDay = [];
+let rowCounter = 0;
+const sum = (accumulator, currentValue) => accumulator + currentValue;
 
-// let hours = ['6:00', '7:00', '8:00', '9:00', '10:00', '11:00', '12:00'];
-
-for(var i = 0; i < locationList.length; i++) {
-    let tBody = document.querySelector('tbody');
-    let tRow = document.createElement('tr');
-    tRow.id = 'tr-' + i.toString();
+for(let i = 0; i < locationList.length; i++) {
+    const tBody = document.querySelector('tbody');
+    const tRow = document.createElement('tr');
+    tRow.id = 'tr-' + rowCounter.toString();
+    tRow.textContent = locationList[i].location;
     tBody.appendChild(tRow);
-    locationName(i);
-    cookiesNeededPerHour(i);
-    //hoursOfOperation(i);
-}
-
-// inserts location name of each cookie shop in table
-function locationName(i) {
-    let tRow = document.querySelector('#tr-' + i.toString());
-    let tData = document.createElement('td');
-    tData.textContent = locationList[i].location;  
-    tRow.appendChild(tData);
-}
-
-// inserts number of cookies needed per hour for each cookie location. 
-function cookiesNeededPerHour(i) {
-    for(var j = 0; j < 15; j++) {
-        let tRow = document.querySelector('#tr-' + i.toString());
-        let tData = document.createElement('td');
-        cookiesPerHour[j] = locationList[i].totalNeeded();
-        tData.textContent = Math.round(cookiesPerHour[j]);
+    
+    for(let j = 0; j < operationHrs.length; j++) {
+        const tRow = document.querySelector('#tr-' + rowCounter.toString());
+        const tData = document.createElement('td');
+        tData.classList = 'td-' + j.toString();
+        const cookiesPerHr = locationList[i].totalNeeded();
+        tData.textContent = cookiesPerHr;
         tRow.appendChild(tData);
+
+        totalCookiesPerDay[j] = cookiesPerHr;
+
     }
+
+    const totalPerDay = document.createElement('td');
+    totalPerDay.textContent = totalCookiesPerDay.reduce(sum);
+    tRow.appendChild(totalPerDay);
+
+    const column = document.querySelector('.td-0').innerHTML;
+    console.log(column);
+
+    // need to get total cookies from all shops per hour
+
+    rowCounter++;
 }
 
-/*
-function hoursOfOperation(i) {
-    for(var k = 0; k < locationList[i].hours.length; k++) {
-        let tRow = document.querySelector('#cookie-header');
-        let tHeader = document.createElement('th');
-        
-        if(hours[k].includes(locationList[i].hours[k])) {
-            tHeader.textContent = locationList[i].hours[k];
-            tRow.appendChild(tHeader);
-        }
-        else {
-            tHeader.textContent = 'CLOSED';
-            tRow.appendChild(tHeader);
-        }
-    }
-} */
+
+
 
 
 
