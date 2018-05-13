5514206c398676733122b8271ca8d5007ca08781
Easton Gorishek <ieaston26@gmail.com>
2018-05-12 19:40:15 -0700

    giving another go

--- app.js
+++ app.js
@@ -1,52 +1,55 @@
-/* globals locationList CookieLocation*/
+/* globals locationList */
 
 const operationHrs = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
-const totalCookiesPerDay = [];
-let rowCounter = 0;
+const rowTotal = [];
 let columnTotal = [];
+let cookieShop = 0;
+
+// used to calculate total # of cookies within array rowTotal and columnTotal
 const sum = (accumulator, currentValue) => accumulator + currentValue;
 
+// 
 function cookieBI(array) {
+    // inserts location of each cookie shop in table
     for(let i = 0; i < array.length; i++) {
         const tBody = document.querySelector('tbody');
         const tRow = document.createElement('tr');
-        tRow.id = 'tr-' + rowCounter.toString();
+        tRow.id = 'tr-' + cookieShop.toString();
         tRow.textContent = array[i].location;
         tBody.appendChild(tRow);
         
+        // inserts the number of cookies needed per hour per shop
         for(let j = 0; j < operationHrs.length; j++) {
-            const tRow = document.querySelector('#tr-' + rowCounter.toString());
-            const tData = document.createElement('td');
-            tData.classList = 'td-' + j.toString();
+            const tRow = document.querySelector('#tr-' + cookieShop.toString());
+            const tCell = document.createElement('td');
+            tCell.classList = 'td-' + j.toString();
             const cookiesPerHr = Math.round(array[i].totalNeeded());
-            tData.textContent = cookiesPerHr;
-            tRow.appendChild(tData);
-            totalCookiesPerDay[j] = cookiesPerHr;
+            rowTotal[j] = cookiesPerHr;
+            tCell.textContent = cookiesPerHr;
+            tRow.appendChild(tCell);
 
+            // inserts the total # of cookies needed per day per shop
             if(j === operationHrs.length - 1) {
                 const totalPerDay = document.createElement('td');
                 totalPerDay.classList = 'td-' + (j + 1).toString();
-                totalPerDay.textContent = totalCookiesPerDay.reduce(sum);
+                totalPerDay.textContent = rowTotal.reduce(sum);
                 tRow.appendChild(totalPerDay);
             }
         }
-        rowCounter++;
+        // keeps count of the # of cookie shops we have
+        cookieShop++;
     }
 
+    // inserts total number of cookies needed between all shops per hour
     for(let i = 0; i < operationHrs.length + 1; i++) {
         for(let k = 0; k < document.querySelectorAll('.td-' + i).length; k++) {
             columnTotal[k] = parseInt(document.querySelectorAll('.td-' + i)[k].innerText);
         }
 
         document.getElementById('tf-' + i.toString()).innerText = columnTotal.reduce(sum);
-        /*
-        const tFooter = document.querySelector('#table-footer');
-        const tCell = document.createElement('td');
-        tCell.textContent = columnTotal.reduce(sum);
-        tFooter.appendChild(tCell); */
     }
 }
-
+// initiates table on page load
 cookieBI(locationList);
 
 function newCookieLocation(e) {
@@ -61,5 +64,5 @@ function newCookieLocation(e) {
     cookieBI(newArray);
 
 }
-const cookieShop = document.getElementById('cookie-form');
-cookieShop.addEventListener('submit', newCookieLocation);
+const newCookieShop = document.getElementById('cookie-form');
+newCookieShop.addEventListener('submit', newCookieLocation);
